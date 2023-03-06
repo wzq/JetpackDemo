0baca4728e99a73baa5c802972865e22761b4eaa
wzq <297001985@qq.com>
2023-03-06 10:12:05 +0800

    update

--- app/src/main/java/com/wzq/sample/ui/MainActivity.kt
+++ app/src/main/java/com/wzq/sample/ui/MainActivity.kt
@@ -2,40 +2,36 @@ package com.wzq.sample.ui
 
 import android.os.Bundle
 import androidx.fragment.app.commit
-import androidx.metrics.performance.JankStats
 import androidx.navigation.fragment.NavHostFragment
 import com.wzq.sample.R
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.asExecutor
-import timber.log.Timber
 
 class MainActivity : BaseActivity() {
 
-    private lateinit var jankState: JankStats
+//    private lateinit var jankState: JankStats
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         bindNavHost()
 
-        jankState = JankStats.createAndTrack(
-            window = window, Dispatchers.Default.asExecutor(), jankFramListener
-        )
+//        jankState = JankStats.createAndTrack(
+//            window = window, Dispatchers.Default.asExecutor(), jankFramListener
+//        )
     }
 
     override fun onResume() {
         super.onResume()
-        jankState.isTrackingEnabled = true
+//        jankState.isTrackingEnabled = true
     }
 
     override fun onPause() {
         super.onPause()
-        jankState.isTrackingEnabled = false
+//        jankState.isTrackingEnabled = false
     }
 
-    private val jankFramListener = JankStats.OnFrameListener {
-        if (it.isJank) Timber.w("jank state frame data --> $it")
-    }
+//    private val jankFramListener = JankStats.OnFrameListener {
+//        if (it.isJank) Timber.w("jank state frame data --> $it")
+//    }
 
     private fun bindNavHost() {
         val hostFragment = NavHostFragment.create(R.navigation.nav_main)
