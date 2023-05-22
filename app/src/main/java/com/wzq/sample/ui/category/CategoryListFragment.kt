bc6912a070cc88810c4981dc300d41b88620b742
wzq <297001985@qq.com>
2023-05-22 17:24:12 +0800

    update

--- app/src/main/java/com/wzq/sample/ui/category/CategoryListFragment.kt
+++ app/src/main/java/com/wzq/sample/ui/category/CategoryListFragment.kt
@@ -1,23 +1,26 @@
 package com.wzq.sample.ui.category
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import androidx.fragment.app.Fragment
+import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
-import com.wzq.sample.R
 import com.wzq.sample.data.paging.CategoryPagerSource
 import com.wzq.sample.databinding.FragmentCategoryListBinding
+import com.wzq.sample.ui.BaseFragment
 import com.wzq.sample.ui.main.home.HomeAdapter
 import com.wzq.sample.util.PAGE_SIZE
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 /**
  * create by wzq on 2021/12/16
  *
  */
-class CategoryListFragment: Fragment(R.layout.fragment_category_list), HomeAdapter.ItemClickListener {
+class CategoryListFragment : BaseFragment(), HomeAdapter.ItemClickListener {
 
     companion object {
         fun newInstance(id: Int): CategoryListFragment {
@@ -29,17 +32,24 @@ class CategoryListFragment: Fragment(R.layout.fragment_category_list), HomeAdapt
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentCategoryListBinding.bind(view)
-        val cid = arguments?.getInt("id") ?: return
+    override fun initView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val cid = arguments?.getInt("id")
+        if (cid == null) {
+            findNavController().navigateUp()
+            return null
+        }
 
+        val binding = FragmentCategoryListBinding.inflate(inflater, container, false)
         val adapter = HomeAdapter(this)
         binding.listview.adapter = adapter
 
-        lifecycleScope.launchWhenStarted {
+        lifecycleScope.launch {
             Pager(
-                PagingConfig(PAGE_SIZE),
+                PagingConfig(PAGE_SIZE, initialLoadSize = PAGE_SIZE),
                 0,
             ) {
                 CategoryPagerSource(cid)
@@ -47,6 +57,7 @@ class CategoryListFragment: Fragment(R.layout.fragment_category_list), HomeAdapt
                 adapter.submitData(it)
             }
         }
+        return binding.root
     }
 
     override fun onItemClick(url: String) {
