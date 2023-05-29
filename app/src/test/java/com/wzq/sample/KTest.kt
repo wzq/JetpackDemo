b9f12c847fd838b0173ddaa1e61f6f227c57bb3d
wzq <297001985@qq.com>
2023-05-29 11:07:26 +0800

    update

--- app/src/test/java/com/wzq/sample/KTest.kt
+++ app/src/test/java/com/wzq/sample/KTest.kt
@@ -1,5 +1,12 @@
 package com.wzq.sample
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 /**
@@ -9,7 +16,24 @@ import org.junit.Test
 class KTest {
 
     @Test
-    fun t1() {
+    fun test() = runTest {
+        GlobalScope.launch {
+            val scope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
 
+            scope.launch {
+                async {
+                    println(1)
+                throw RuntimeException("task1 failed")
+                }.join()
+                async {
+                    println(3)
+                    println(33)
+                }.join()
+                async {
+                    println(4)
+                    println(44)
+                }.join()
+            }
+        }
     }
 }
