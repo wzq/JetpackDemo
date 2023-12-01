42a140fd9ed54c4d7862ab57d011eec5c3ce7eed
wzq <wzq_1990@sina.com>
2023-12-01 10:00:18 +0800

    update

--- app/src/test/java/com/wzq/jd/compose/app/ExampleUnitTest.kt
+++ app/src/test/java/com/wzq/jd/compose/app/ExampleUnitTest.kt
@@ -1,5 +1,8 @@
 package com.wzq.jd.compose.app
 
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +16,51 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun test() = runBlocking {
+
+        val supervisorJob = SupervisorJob()
+
+        val parentJob = launch(supervisorJob) {
+            val childJob1 = launch {
+                // 子协程1的操作
+                println(1)
+            }
+
+            val childJob2 = launch {
+                // 子协程2的操作，可能会失败
+                throw Exception("Child job 2 failed")
+            }
+
+            val job3 = launch {
+                println(3)
+            }
+        }
+
+        parentJob.join()
+
+        Unit
+    }
+
+
+    @Test
+    fun goodTest() = runBlocking {
+        val job = SupervisorJob()
+
+        launch(job) {
+            println(1)
+        }
+
+        launch(job) {
+            throw Exception("job2")
+        }
+
+        launch(job) {
+            println(3)
+        }
+
+        Unit
     }
 }
